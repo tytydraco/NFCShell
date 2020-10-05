5331825dc724b22dca8a00cf624a6d99b5dc9d67
tytydraco <tylernij@gmail.com>
2020-10-05 16:11:13 -0700

    Simplify code
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/nfcshell/ShellActivity.kt
+++ app/src/main/java/com/draco/nfcshell/ShellActivity.kt
@@ -2,77 +2,26 @@ package com.draco.nfcshell
 
 import android.app.Activity
 import android.os.Bundle
-import android.os.PowerManager
-import java.io.File
+import java.io.FileOutputStream
 
 class ShellActivity : Activity() {
-    private lateinit var nfc: Nfc
-    private lateinit var execution: Execution
-
-    /* Choose the best accessible working directory for the script */
-    private fun getBestWorkingDir(): File {
-        val externalFilesDir = getExternalFilesDir(null)
-
-        /* ~/Android/data/id/files */
-        if (externalFilesDir != null)
-            return externalFilesDir
-
-        /* ~/Android/data/id/cache */
-        if (externalCacheDir != null)
-            return externalCacheDir!!
-
-        /* /data/data/id/files */
-        return filesDir
-    }
-
-    /* Clean and prepare working directory and return its path */
-    private fun prepareWorkingDir(delete: Boolean = true): File {
-        /* Find best choice of working dir */
-        val workingDir = getBestWorkingDir()
-
-        /* Delete everything in it */
-        if (delete)
-            workingDir.deleteRecursively()
-
-        /* Recreate clean working dir */
-        workingDir.mkdirs()
-
-        return workingDir
-    }
-
-    /* First write out script to internal storage, then execute it */
-    private fun executeScriptFromBytes(bytes: ByteArray) {
-        /* Do not allow concurrent modifications */
-        if (execution.executing.get())
-            return
-
-        /* Setup our execution environment */
-        val execParams = ExecParams()
-        with (execParams) {
-            scriptBytes = bytes
-            workingDir = prepareWorkingDir()
-        }
-
-        /* Execute script in another thread */
-        Thread {
-            execution.execute(execParams)
-        }.start()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         finish()
 
         /* Register our Nfc helper class */
-        nfc = Nfc(this)
-
-        /* Create our execution environment */
-        execution = Execution(getSystemService(POWER_SERVICE) as PowerManager)
+        val nfc = Nfc(this)
 
         /* Get tag contents */
         val bytes = nfc.readBytes(intent)
 
-        /* Execute */
-        executeScriptFromBytes(bytes)
+        /* Write our script */
+        val fileOutput = createTempDir("script", "sh")
+        val fileOutputStream = FileOutputStream(fileOutput)
+        fileOutputStream.write(bytes)
+        fileOutputStream.close()
+
+        /* Begin execution */
+        ProcessBuilder("sh", fileOutput.absolutePath).start()
     }
 }
